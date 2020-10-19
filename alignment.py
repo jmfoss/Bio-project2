fad975f05b3b4fdb32aaae512865108b30abeabc
jmfoss <jmfoss98@gmail.com>
2020-10-19 13:19:14 -0400

    Add loading function for our gene

--- alignment.py
+++ alignment.py
@@ -1,1 +1,15 @@
+def main():
+    n_gene = get_gene(28274, 29533)
 
+
+# param: start and end index of dna segment
+# return: string of rna segment 3'-5'
+def get_gene(begin, end):
+    sequence = open("sequence.fasta", "r").read()
+    sequence = sequence.replace("\n", "")
+    return sequence[begin - 1: end]
+
+
+# Causes the program to run main when script is ran
+if __name__ == '__main__':
+    main()
