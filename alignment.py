15ec6276e67edcd0b2d3865060783536b9e5feea
jmfoss <jmfoss98@gmail.com>
2020-10-19 13:27:57 -0400

    Add covid loading sequence

--- alignment.py
+++ alignment.py
@@ -1,11 +1,11 @@
 def main():
-    n_gene = get_gene(28274, 29533)
-
+    n_mers_gene = get_gene(28274, 29533, "MERS_sequence.txt")
+    n_covid_gene = get_gene(28274, 29533, "COVID_sequence.txt")
 
 # param: start and end index of dna segment
 # return: string of rna segment 3'-5'
-def get_gene(begin, end):
-    sequence = open("sequence.txt", "r").read()
+def get_gene(begin, end, filename):
+    sequence = open(filename, "r").read()
     sequence = sequence.replace("\n", "")
     return sequence[begin - 1: end]
 
